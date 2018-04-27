8b3284b0f44e56677b7a9fab129677eb1beb348b
bonnux <sa517111@mail.ustc.edu.cn>
2018-04-27 12:31:44 +0800

    2018.4.27

--- src/main/java/com/bonnux/wenda/service/UserService.java
+++ src/main/java/com/bonnux/wenda/service/UserService.java
@@ -1,6 +1,8 @@
 package com.bonnux.wenda.service;
 
+import com.bonnux.wenda.dao.LoginTicketDAO;
 import com.bonnux.wenda.dao.UserDAO;
+import com.bonnux.wenda.model.LoginTicket;
 import com.bonnux.wenda.model.User;
 import com.bonnux.wenda.util.WendaUtil;
 import org.slf4j.Logger;
@@ -9,11 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.apache.commons.lang.StringUtils;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
-import java.util.UUID;
-
+import java.util.*;
 
 @Service
 public class UserService {
@@ -21,34 +19,90 @@ public class UserService {
     @Autowired
     private UserDAO userDAO;
 
-    public Map<String,String> register(String username, String password){
-        Map<String, String> map = new HashMap<>();
-        if (StringUtils.isBlank(username)){
-            map.put("msg","用户名不能为空");
+    @Autowired
+    private LoginTicketDAO loginTicketDAO;
+
+    public Map<String, Object> register(String username, String password) {
+        Map<String, Object> map = new HashMap<String, Object>();
+        if (StringUtils.isBlank(username)) {
+            map.put("msg", "用户名不能为空");
+            return map;
         }
 
-        if (StringUtils.isBlank(password)){
-            map.put("msg","密码不能为空");
+        if (StringUtils.isBlank(password)) {
+            map.put("msg", "密码不能为空");
             return map;
         }
 
         User user = userDAO.selectByName(username);
-        if (user!=null){
+
+        if (user != null) {
             map.put("msg", "用户名已经被注册");
             return map;
         }
 
+        // 密码强度
         user = new User();
         user.setName(username);
-        user.setSalt(UUID.randomUUID().toString().substring(0,5));
+        user.setSalt(UUID.randomUUID().toString().substring(0, 5));
         String head = String.format("http://images.nowcoder.com/head/%dt.png", new Random().nextInt(1000));
+        user.setHeadUrl(head);
         user.setPassword(WendaUtil.MD5(password+user.getSalt()));
         userDAO.addUser(user);
 
+        // 登陆
+        String ticket = addLoginTicket(user.getId());
+        map.put("ticket", ticket);
         return map;
+    }
+
+
+    public Map<String, Object> login(String username, String password) {
+        Map<String, Object> map = new HashMap<String, Object>();
+        if (StringUtils.isBlank(username)) {
+            map.put("msg", "用户名不能为空");
+            return map;
+        }
+
+        if (StringUtils.isBlank(password)) {
+            map.put("msg", "密码不能为空");
+            return map;
+        }
+
+        User user = userDAO.selectByName(username);
+
+        if (user == null) {
+            map.put("msg", "用户名不存在");
+            return map;
+        }
+
+        if (!WendaUtil.MD5(password+user.getSalt()).equals(user.getPassword())) {
+            map.put("msg", "密码不正确");
+            return map;
+        }
+
+        String ticket = addLoginTicket(user.getId());
+        map.put("ticket", ticket);
+        return map;
+    }
+
+    private String addLoginTicket(int userId) {
+        LoginTicket ticket = new LoginTicket();
+        ticket.setUserId(userId);
+        Date date = new Date();
+        date.setTime(date.getTime() + 1000*3600*24);
+        ticket.setExpired(date);
+        ticket.setStatus(0);
+        ticket.setTicket(UUID.randomUUID().toString().replaceAll("-", ""));
+        loginTicketDAO.addTicket(ticket);
+        return ticket.getTicket();
     }
 
     public User getUser(int id) {
         return userDAO.selectById(id);
+    }
+
+    public void logout(String ticket) {
+        loginTicketDAO.updateStatus(ticket, 1);
     }
 }
--- src/main/java/com/bonnux/wenda/service/WendaService.java
+++ src/main/java/com/bonnux/wenda/service/WendaService.java
@@ -2,9 +2,6 @@ package com.bonnux.wenda.service;
 
 import org.springframework.stereotype.Service;
 
-/**
- * Created by bonnux on 2018/3/10.
- */
 @Service
 public class WendaService {
     public String getMessage(int userId) {
