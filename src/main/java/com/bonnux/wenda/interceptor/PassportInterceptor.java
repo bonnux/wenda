b355c60f6122b8bb627e1bf347d46a996d80dd08
bonnux <sa517111@mail.ustc.edu.cn>
2018-04-28 16:48:12 +0800

    2018.4.28

---
+++ src/main/java/com/bonnux/wenda/interceptor/PassportInterceptor.java
@@ -0,0 +1,62 @@
+package com.bonnux.wenda.interceptor;
+
+import com.bonnux.wenda.dao.LoginTicketDAO;
+import com.bonnux.wenda.dao.UserDAO;
+import com.bonnux.wenda.model.HostHolder;
+import com.bonnux.wenda.model.LoginTicket;
+import com.bonnux.wenda.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+
+@Component
+public class PassportInterceptor implements HandlerInterceptor {
+    @Autowired
+    LoginTicketDAO loginTicketDAO;
+
+    @Autowired
+    private UserDAO userDAO;
+
+    @Autowired
+    HostHolder hostHolder;
+
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        String ticket = null;
+        if (httpServletRequest.getCookies() != null) {
+            for (Cookie cookie : httpServletRequest.getCookies()) {
+                if (cookie.getName().equals("ticket")) {
+                    ticket = cookie.getValue();
+                    break;
+                }
+            }
+        }
+
+        if (ticket != null) {
+            LoginTicket loginTicket = loginTicketDAO.selectByTicket(ticket);
+            if (loginTicket == null || loginTicket.getExpired().before(new Date()) || loginTicket.getStatus() != 0) {
+                return true;
+            }
+
+            User user = userDAO.selectById(loginTicket.getUserId());
+            hostHolder.setUser(user);
+        }
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+}
