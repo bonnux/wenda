cbcf91f1b12349b828b5a335ceb952b640fb3be8
bonnux <sa517111@mail.ustc.edu.cn>
2018-05-28 20:17:49 +0800

    2018.5.28

--- src/main/java/com/bonnux/wenda/model/HostHolder.java
+++ src/main/java/com/bonnux/wenda/model/HostHolder.java
@@ -4,8 +4,10 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class HostHolder {
+    //每个线程都有一份拷贝，在不同的内存空间
     private static ThreadLocal<User> users = new ThreadLocal<User>();
 
+    //根据当前的线程，获取当前线程的user，相当于一个Map<ThreadId,User>
     public User getUser() {
         return users.get();
     }
